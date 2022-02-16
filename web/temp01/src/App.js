dffd25736ed7da996b9c3708aedcb05976238074
Игорь <ogigora@mail.ru>
2022-02-16 15:56:27 +0500

    alalalal

--- web/temp01/src/App.js
+++ web/temp01/src/App.js
@@ -1,10 +1,19 @@
 import './App.css';
+import lupa from "./img/lupa.jpg"
 
 function App() {
   return (
     <div className="App">
-     проверка
+        <a className="way-way" href="https://yandex.ru/soft/win/startpage?from=prov_yaru&banerid=">Сделать стартовой</a>
+        <img className="image-txt" src={lupa} alt="nothing"/>
+        <a className="yan-dex" target="_blank" href="//yandex.ru">ЯНДЕКС</a>
+        <div className="search-box">
+            <input className="search-txt" type="text" name="" placeholder="Type to search"/>
+        </div>
+
+
     </div>
+
   );
 }
 
