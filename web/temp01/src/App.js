d4528f0bbddc4a01a0b0b65ff4f98f315472342e
Игорь <ogigora@mail.ru>
2022-02-18 14:37:46 +0500

    333

--- web/temp01/src/App.js
+++ web/temp01/src/App.js
@@ -4,11 +4,14 @@ import lupa from "./img/lupa.jpg"
 function App() {
   return (
     <div className="App">
-        <a className="way-way" href="https://yandex.ru/soft/win/startpage?from=prov_yaru&banerid=">Сделать стартовой</a>
-        <img className="image-txt" src={lupa} alt="nothing"/>
-        <a className="yan-dex" target="_blank" href="//yandex.ru">ЯНДЕКС</a>
-        <div className="search-box">
-            <input className="search-txt" type="text" name="" placeholder="Type to search"/>
+        <div className="top">
+            TOP
+        </div>
+        <div className="mid">
+            MID
+        </div>
+        <div className="bot">
+            BOT
         </div>
 
 
