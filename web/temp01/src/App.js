a38a812922b8e1e1e19f7cb56caf99600eddc995
Игорь <ogigora@mail.ru>
2022-02-20 19:58:28 +0500

    333

--- web/temp01/src/App.js
+++ web/temp01/src/App.js
@@ -5,13 +5,21 @@ function App() {
   return (
     <div className="App">
         <div className="top">
-            TOP
+            <div className="left">
+                <a className="box" href="https://yandex.ru/soft/win/startpage?from=prov_yaru">Сделать стартовой</a>
+            </div>
+            <div className="right">
+                <a className="di1"  href="https://passport.yandex.ru/passport?mode=passport"> Пользователь</a>
+                <a className="di2"  href="https://mail.yandex.ru/">Почта</a>
+                <a className="di3"  href="https://ya.ru/">Выход</a>
+            </div>
         </div>
         <div className="mid">
-            MID
+            <input className="search" placeholder="Искать здесь..." type="search"/>
+
         </div>
         <div className="bot">
-            BOT
+            <a className="floot" href="https://ya.ru/"> </a>
         </div>
 
 
