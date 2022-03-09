3af59e6a175d61d32d1c2ca54136ab90b046b4c9
Игорь <ogigora@mail.ru>
2022-03-09 19:52:18 +0500

    333

--- web/temp01/src/App2.js
+++ web/temp01/src/App2.js
@@ -2,36 +2,25 @@ import './App2.css';
 import {useState} from "react";
 function App2() {
 
-
-
-const [inputText, setInputText] = useState('')
+    const [inputText, setInputText] = useState('')
     const [result, setResult] = useState('')
 
-const handler = (zxc) =>{
-    setInputText(zxc.target.value)
-}
-const fotler =() =>{
-    setResult(inputText)
-}
+    const handler = (zxc) =>{
+        setInputText(zxc.target.value)
+    }
+    const foot = () => {
+        setResult(inputText)
+    }
 
-
-    return<div className='App2'>
-
-    <div className='vvod'>
-        <input type ='text' onChange={handler}/>
-
-    <button type = 'sumbit' onClick={fotler
-    }>PUSH
-    </button>
+    return <div
+        className='App2'>
+        <div className='input'>
+            <input type ='text' onChange = {handler}/>
+            <button type = 'submit' onClick = {foot}
+            >PUSH
+            </button>
+        </div>
+           <textarea className = 'area' defaultValue = {result}/>
     </div>
-           <textarea className='area' defaultValue={result} />
-
-    </div>
-
-
-
-
-
-
 }
 export default App2
